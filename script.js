f2a19428d203f884af0f726b33f5931f59d66db6
shivshankar-source <shivshakti.ss771@gmail.com>
2025-09-27 00:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+//Write your code here 
+let table=document.getElementById("sampleTable")
+let button=document.getElementById("addRow");
+
+button.addEventListener("click",insert_Row)
+
 function insert_Row() {
-    //Write your code here
-  
-  
+let tr=document.createElement("tr")
+let td1=document.createElement("td")
+td1.innerText="New Cell1"
+let td2=document.createElement("td")
+td2.innerText="New Cell2"
+tr.append(td1,td2)
+table.prepend(tr)
 }
+
+
