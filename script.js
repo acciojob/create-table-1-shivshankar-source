e66a842f19ea035f046489353d51e4f2dddaf6da
shivshankar-source <shivshakti.ss771@gmail.com>
2025-11-11 11:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-//Write your code here 
-let table=document.getElementById("sampleTable")
-let button=document.getElementById("addRow");
 
-button.addEventListener("click",insert_Row)
 
+let table = document.getElementById("sampleTable");
+
+	let count=3;
 function insert_Row() {
+
 let tr=document.createElement("tr")
 let td1=document.createElement("td")
-td1.innerText="New Cell1"
+td1.innerText=`Row${count} Cell1`
+
 let td2=document.createElement("td")
-td2.innerText="New Cell2"
+td2.innerText=`Row${count} Cell2`
 tr.append(td1,td2)
-table.prepend(tr)
+
+table.append(tr)
+	count++;
 }
 
 
