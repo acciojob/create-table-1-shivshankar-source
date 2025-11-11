54b5f0fe9c2949fb462a34096f28c6ad5900ddbf
shivshankar-source <shivshakti.ss771@gmail.com>
2025-11-11 11:41:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,17 @@
 
 let table = document.getElementById("sampleTable");
 
-	let count=3;
 function insert_Row() {
 
 let tr=document.createElement("tr")
 let td1=document.createElement("td")
-td1.innerText=`Row${count} Cell1`
+td1.innerText=`New Cell1`
 
 let td2=document.createElement("td")
-td2.innerText=`Row${count} Cell2`
+td2.innerText=`New Cell2`
 tr.append(td1,td2)
 
-table.append(tr)
-	count++;
+table.prepend(tr)
 }
 
 
